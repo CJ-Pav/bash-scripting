ff2ce1456aaf696ea06c41e4812b1394656d28ab
CJ-Pav <cpav@live.com>
2019-04-15 13:49:42 -0700

    format updates

--- updates/update-manjaro.sh
+++ updates/update-manjaro.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 # update script for Manjaro Linux
 
-# echo "Grabbing fastest mirrors..."
-# sudo pacman-mirrors --fasttrack
+# prompt to update selected mirrors
+read -p "Would you like to select a faster mirror before updating? [y/N]: " -r
+if [[ "$REPLY" =~ ^[yY] ]]; then
+    echo "Grabbing fastest mirrors..."
+    sudo pacman-mirrors --fasttrack
+fi
 
-echo "Checking for & running updates..."
+echo; echo "Updating..."; echo
 yaourt -Syua
 
-# clean up
-yaourt -Qdt
-yaourt -Rs $(pacman -Qtdq)
+# prompt for clean up
+read -p "Would you like to remove non-essential packages? [y/N]: " -r
+if [[ "$REPLY" =~ ^[yY] ]]; then
+    yaourt -Qdt
+    yaourt -Rs $(pacman -Qtdq)
+fi
 
-echo "Complete"
+echo; echo "Complete."; echo
+
+# prompt for system restart
+read -p "Reboot recommended, do this now? [y/N]: " -r
+if [[ "$REPLY" =~ ^[yY] ]]; then
+    sudo reboot
+fi
+
+exit 0 || return 0
--- updates/update-mint.sh
+++ updates/update-mint.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 # update script for mint
 
-echo
-echo "Updating system..."
+echo; echo "Updating system..."
 
 sudo apt update -y
 sudo apt upgrade -y
 
-echo
-echo "Cleaning up..."
+echo; echo "Cleaning up..."
 
 sudo apt autoremove -y
 sudo apt clean metadata
 
-echo
-echo "Complete"
-echo
-
+echo; echo "Complete."
